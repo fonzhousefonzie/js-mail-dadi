a462b828f28369b7aa499292726982a4487b9115
fonzhousefonzie <harth.fonz@gmail.com>
2022-05-23 20:18:11 +0200

    super extra bonus

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -9,17 +9,21 @@ array, anche se non conosciamo a priori quanti elementi contiene.
 Extra Bonus
 Creiamo un array chiedendo all’utente quanti elementi dovrà contenere e generando tanti 
 numeri interi casuali quanti sono gli elementi da inserire.
-Attenzione #3: quali controlli dobbiamo fare sull’input dell’utente?
+
 Super Extra Bonus: chiedere all’utente anche quanti elementi vanno estratti dal fondo 
 dell’array.
 */
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 const element = document.getElementById('element');
 
-//const quantiElementi = prompt(`Quanti elementi deve contenere l'array?`);
-const quantiStampati = prompt(`Quanti elementi vuoi stampare, partendo dalla fine?`);
+const quantiElementi = parseInt(prompt(`Quanti elementi deve contenere l'array?`));
+const quantiStampati = parseInt(prompt(`Quanti elementi vuoi stampare, partendo dalla fine?`));
+
+const array = [];
+
+for (let x = 1; x <= quantiElementi; x++){
+    array.push(x);
+}
 
 for (let i = (array.length - quantiStampati); i < array.length; i++){
     element.innerHTML+= `<p>${array[i]}</p>`;
